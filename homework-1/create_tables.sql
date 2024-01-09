ca7cce51a0d2eae0c7e8c25902cde0a8636cbd7d
gelena <helenatcitok@gmail.com>
2024-01-09 10:42:31 +0000

    initial

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NUll,
+	birth_date date,
+	notes text
+);
+
+SELECT * FROM employees
+
+CREATE TABLE customers
+(
+	customer_id varchar(50) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT null
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) NOT NULL,
+	employee_id int,
+	order_date date,
+	ship_city varchar(20)
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,65 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+from psycopg2 import sql
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="2182"
+)
+
+cur = conn.cursor()
+
+employees_data = [
+    (1, 'Nancy', 'Davolio', 'Sales Representative', '1948-12-08', 'Education includes a BA in psychology from '
+                                                                  'Colorado State University in 1970. She also '
+                                                                  'completed The Art of the Cold Call. Nancy is a '
+                                                                  'member of Toastmasters International.'),
+    (2, 'Andrew', 'Fuller', 'Vice President, Sales', '1952-02-19', 'Andrew received his BTS commercial in 1974 and a '
+                                                                   'Ph.D. in international marketing from the '
+                                                                   'University of Dallas in 1981. He is fluent in '
+                                                                   'French and Italian and reads German. He joined '
+                                                                   'the company as a sales representative, '
+                                                                   'was promoted to sales manager in January 1992 and '
+                                                                   'to vice president of sales in March 1993. Andrew '
+                                                                   'is a member of the Sales Management Roundtable, '
+                                                                   'the Seattle Chamber of Commerce, and the Pacific '
+                                                                   'Rim Importers Association.'),
+]
+
+insert_query = sql.SQL(
+    "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES {}"
+     ).format(
+    sql.SQL(',').join(map(sql.Literal, employees_data))
+)
+
+cur.execute(insert_query)
+
+customers_data = [
+    ('ALFKI', 'Alfreds Futterkiste', 'Maria Anders'),
+    ('ANATR', 'Ana Trujillo Emparedados y helados', 'Ana Trujillo'),
+]
+
+insert_query = sql.SQL("INSERT INTO customers (customer_id, company_name, contact_name) VALUES {}").format(
+    sql.SQL(',').join(map(sql.Literal, customers_data))
+)
+
+cur.execute(insert_query)
+
+orders_data = [
+    (10248, 'VINET', 5, '1996-07-04', 'Reims'),
+    (10249, 'TOMSP', 6, '1996-07-05', 'Münster'),
+]
+
+insert_query = sql.SQL(
+    "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES {}"
+     ).format(
+    sql.SQL(',').join(map(sql.Literal, orders_data))
+)
+
+cur.execute(insert_query)
+
+conn.commit()
+cur.close()
+conn.close()

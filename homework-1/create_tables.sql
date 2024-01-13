1060b0c979a957619f48e2be965344e3f458c71a
gelena <helenatcitok@gmail.com>
2024-01-13 20:44:40 +0000

    HW1 fixing 1 more time

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,28 +1,26 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE employees
 (
-	employee_id int PRIMARY KEY,
-	first_name varchar(100) NOT NULL,
+    employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
 	last_name varchar(100) NOT NULL,
-	title varchar(100) NOT NUll,
-	birth_date date,
-	notes text
+	title varchar(100) NOT NULL,
+	birth_date varchar(100) NOT NULL,
+	notes varchar(500) NOT NULL
 );
-
-SELECT * FROM employees
 
 CREATE TABLE customers
 (
-	customer_id varchar(50) PRIMARY KEY,
-	company_name varchar(50) NOT NULL,
-	contact_name varchar(50) NOT null
+    customer_id varchar(10) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
 );
 
 CREATE TABLE orders
 (
-	order_id int PRIMARY KEY,
-	customer_id varchar(50) NOT NULL,
-	employee_id int,
-	order_date date,
-	ship_city varchar(20)
+    order_id int PRIMARY KEY,
+    customer_id varchar(10) NOT NULL REFERENCES customers(customer_id),
+	employee_id int NOT NULL REFERENCES employees(employee_id),
+	order_date varchar(100) NOT NULL,
+	ship_city varchar(100) NOT NULL
 )
--- homework-1/main.py
+++ homework-1/main.py
@@ -14,17 +14,31 @@ def insert_datas_to_sql(filename, tablename):
             with psycopg2.connect(host="localhost", database="north", user="postgres", password="2182") as conn:
                 with conn.cursor() as curs:
                     for row in data:
+                        customer_id = row['customer_id']
+                        if not isinstance(customer_id, int):
+                            # Handle invalid customer_id value
+                            print(f"Invalid customer_id value: '{customer_id}'")
+                            break
+
+                        customer_id_int = int(customer_id)
                         row_data = []
                         values_designation = []
-                        for key in list(row.keys()):
-                            row_data.append(row[key])
+                        for key, value in row.items():
+                            if key == "customer_id":
+                                customer_id_int = int(customer_id)
+                                row_data.append(customer_id_int)
+                            else:
+                                row_data.append(value)
                             values_designation.append('%s')
                         curs.execute(f"INSERT INTO {tablename} VALUES ({', '.join(values_designation)})",
                                      tuple(row_data))
                         row_data.clear()
                         values_designation.clear()
+    except psycopg2.OperationalError as e:
+        print(f"Error: {e}")
     finally:
-        conn.close()
+        if conn:
+            conn.close()
 
 
 insert_datas_to_sql(FILE_CUSTOMERS_DATA, 'orders')
